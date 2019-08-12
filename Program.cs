5b84791944c4752b344c1367a635ba81560ae93f
moh <osman.elrashid@gmail.com>
2019-08-12 14:12:21 +0400

    initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace e040_csharp_console_paypal_d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e040_csharp_console_paypal_d 
+git push --set-upstream origin master 
